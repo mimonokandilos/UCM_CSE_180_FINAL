e690c731403161b986754991136aa05e959126fa
mimonokandilos <monokandilos.m@gmail.com>
2023-04-18 18:34:41 -0700

    first of many commits

---
+++ CSE180/src/project/src/navigate.cpp
@@ -0,0 +1,74 @@
+#include <rclcpp/rclcpp.hpp> 
+#include <navigation/navigation.hpp>
+#include <iostream>
+
+
+
+int main(int argc,char **argv) {
+ 
+  rclcpp::init(argc,argv); // initialize ROS 
+  Navigator navigator(true,false); // create node with debug info but not verbose
+
+  // first: it is mandatory to initialize the pose of the robot
+  geometry_msgs::msg::Pose::SharedPtr init = std::make_shared<geometry_msgs::msg::Pose>();
+  init->position.x = -2;
+  init->position.y = -0.5;
+  init->orientation.w = 1;
+  navigator.SetInitialPose(init);
+  // wait for navigation stack to become operationale
+  navigator.WaitUntilNav2Active();
+  // spin in place of 90 degrees (default parameter)
+  navigator.Spin();
+  while ( ! navigator.IsTaskComplete() ) {
+    // busy waiting for task to be completed
+  }
+  geometry_msgs::msg::Pose::SharedPtr goal_pos = std::make_shared<geometry_msgs::msg::Pose>();
+  
+  float arr_loc_x[] = {-1, 0, 1, 1.5, 1.5, 1.5, 1, 0, -1, -2};
+  float arr_loc_y[] = {-2, -2, -2, -1, 0, 1, 1.75, 2, 2, 0.5};
+  
+  // var for the location array indecies
+  int arr_index = 0;
+  
+  // Loop until we either visit all locations or find the goal
+  while (arr_index != 10)
+  {
+  	goal_pos->position.x = arr_loc_x[arr_index];
+  	goal_pos->position.y = arr_loc_y[arr_index];
+  	goal_pos->orientation.w = 1;
+  	// move to new pose
+  	navigator.GoToPose(goal_pos);
+  	while ( ! navigator.IsTaskComplete() ) {
+  	  	
+  	}
+  	
+  	// Increment arr_index
+  	arr_index++;
+   }
+   
+  float arr_loc_x_two[] = {-0.5, 0.5, 0.5, -0.5};
+  float arr_loc_y_two[] = {0.5, 0.5, -0.5, -0.5};
+  
+  // var for the location array indecies
+  arr_index = 0;
+  
+  // Loop until we either visit all locations or find the goal
+  while (arr_index != 4)
+  {
+  	goal_pos->position.x = arr_loc_x_two[arr_index];
+  	goal_pos->position.y = arr_loc_y_two[arr_index];
+  	goal_pos->orientation.w = 1;
+  	// move to new pose
+  	navigator.GoToPose(goal_pos);
+  	while ( ! navigator.IsTaskComplete() ) {
+  	  	
+  	}
+  	
+  	// Increment arr_index
+  	arr_index++;
+   }
+  // complete here....
+  
+  rclcpp::shutdown(); // shutdown ROS
+  return 0;
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+# Intro To Robotics
+### UCM_CSE_180_FINAL
+### Author: Mike Monokandilos, Milan Overholtzer, Jerin Sajimon
+### Instructor: StefanoCarpin
+
+# Creating a Workspace
+- 
+    1. mkdir -p CSE180/src
+    2. colcon build
+    3. . install/local_setup.bash
+
+# Creating a Package
+-  
+    1. ros2 pkg create [name_of_pkg]
+    2. ros2 pkg create --build-type ament_cmake [name_of_pkg]
+
+# Download ubuntu at this link:
+https://www.releases.ubuntu.com/focal/
+
+# Download foxy at this link:
+https://docs.ros.org/en/foxy/Installation/Alternatives/Ubuntu-Development-Setup.html
+
+
+#   Instructions from STEFANO CARPIN:
+-   1. Install ROS2 Foxy: follow instructions for Desktop Install on https://docs.ros.org/en/foxy/  Installation/Ubuntu-Install-Debians.html.
+    2. Install Gazebo: sudo apt install ros-foxy-gazebo-*
+    3. Install the turtlebot packages: sudo apt install ros-foxy-turtlebot3* (you can skip this step if you do the optional step described below)
+    4. Install the nav2 packages: sudo apt install ros-foxy-navigation2 ros-foxy-nav2-bringup (see also https://navigation.ros.org/build_instructions/index.html for more details)
+    5. Install colcon: follow instructions on https://colcon.readthedocs.io/en/released/user/installation.html
+    6. Optional: Install additional ROS2 packages: sudo apt install ros-foxy-* (be patient -- this will take some time and a lot of space!).
+    7. It is advised that you add the following command to your startup scripts (e.g., .basrc) source /opt/ros/foxy/setup.bash
+
+
+# LAB07
+- not sure if this is what we are supposed to use
+export TURTLEBOT3_MODEL=waffle
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/foxy/share/turtlebot3_gazebo/models
+ros2 launch gazeboenvs tb3_simulation.launch.py 
